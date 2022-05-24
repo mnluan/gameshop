65ba21f18726e6db86ce2ccb7bf76c388cb26507
Luan Nascimento <luann@id.uff.br>
2022-05-24 04:03:58 -0300

    Add responsive responsive function

--- js/script.js
+++ js/script.js
@@ -6,29 +6,36 @@ setInterval( ()=>{
 }, 5000);
 
 function nextInfo(){
+
     if(document.getElementById("radio1").checked){
-        document.getElementById('title').innerHTML = 'Cyberpunk 2077';
+        document.getElementById('title').innerHTML = 'Cyberpunk <br>2077';
         document.getElementById('price-slide').innerHTML = 'US$ 35,00';
+        document.getElementById("sliderALT").innerHTML = '<img src="./img/slides/01.jpg">';
     }
     if(document.getElementById("radio2").checked){
-        document.getElementById('title').innerHTML = 'Sifu';
+        document.getElementById('title').innerHTML = 'SIFU';
         document.getElementById('price-slide').innerHTML = 'US$ 22,90';
+        document.getElementById("sliderALT").innerHTML = '<img src="./img/slides/02.jpg">';
     }
     if(document.getElementById("radio3").checked){
         document.getElementById('title').innerHTML = 'The Elder Scrolls V: Skyrim';
         document.getElementById('price-slide').innerHTML = 'US$ 19,99';
+        document.getElementById("sliderALT").innerHTML = '<img src="./img/slides/03.jpg">';
     }
     if(document.getElementById("radio4").checked){
         document.getElementById('title').innerHTML = 'Elden Ring';
         document.getElementById('price-slide').innerHTML = 'US$ 42,00';
+        document.getElementById("sliderALT").innerHTML = '<img src="./img/slides/04.jpg">';
     }
     if(document.getElementById("radio5").checked){
-        document.getElementById('title').innerHTML = 'EA Sports FC';
+        document.getElementById('title').innerHTML = 'EA Sports <br>FIFA 22';
         document.getElementById('price-slide').innerHTML = 'US$ 39,99';
+        document.getElementById("sliderALT").innerHTML = '<img src="./img/slides/05.jpg">';
     }
     if(document.getElementById("radio6").checked){
         document.getElementById('title').innerHTML = 'HOTWHEELS: Unleashed';
         document.getElementById('price-slide').innerHTML = 'US$ 17,99';
+        document.getElementById("sliderALT").innerHTML = '<img src="./img/slides/06.jpg">';
     }
 };
 
