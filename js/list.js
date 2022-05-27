6c6d02aabfd26f2e8532cd1dd031307c19d7f297
Luan Nascimento <luann@id.uff.br>
2022-05-26 22:10:41 -0300

    created all games page

---
+++ js/list.js
@@ -0,0 +1,15 @@
+function listgames(){
+fetch(`https://mnluan.github.io/gameshop/db/games.json`)
+.then((res) => res.json())
+.then((data) => {
+    for (i = 0; i < data.length; i++){
+        document.getElementById('list-all').innerHTML += '<div class="game-item">' + 
+        '<img class="logo" src='+ data[i] + '>' +
+        '<div class="title">' + data[i].Image; +
+        '</div>'
+    }
+    }
+    )
+};
+
+listgames();
