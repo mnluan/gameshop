a3844e5cb31f6866606c21ffd3e0bb661e0cd2e6
Luan Nascimento <luann@id.uff.br>
2022-05-31 20:55:14 -0300

    added function that lets you search by pressing enter or typing a word in the search bar

--- js/search.js
+++ js/search.js
@@ -1,10 +1,46 @@
 var input = document.getElementById("searchbar");
 
+function closebtn(){
+  document.getElementById("search_result").style.display = "none"; 
+}
+
+function autoSearch(){
+  var x = document.getElementById("searchbar").value.toUpperCase();
+  if((x == "") || (x == " ")){
+    document.getElementById("search_result").style.display = "none";
+    document.getElementById('result').innerHTML = "";
+  }else{
+    fetch(`https://mnluan.github.io/gameshop/db/games.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        for (i = 0; i < data.length; i++){
+          document.getElementById("search_result").style.display = "block"; 
+          var string = data[i].name.toUpperCase();
+          if((string.includes(x)) && ((x != "") && (x != " "))){
+            var url = data[i]._id.$oid
+            document.getElementById('result').innerHTML = '<div><a href="https://mnluan.github.io/gameshop/game/' + url + '" target="_blank">'+ data[i].name +'</a></div>';
+          }
+        }
+      });
+  }
+}
+
 input.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
       event.preventDefault();
-      var x = document.getElementById("searchbar").value;
-      alert("value " + x + "!")
+      document.getElementById('result').innerHTML = "";
+      var x = document.getElementById("searchbar").value.toUpperCase();
+      fetch(`https://mnluan.github.io/gameshop/db/games.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        for (i = 0; i < data.length; i++){
+          var string = data[i].name.toUpperCase();
+          if((string.includes(x)) && ((x != "") && (x != " "))){
+            var url = data[i]._id.$oid
+            document.getElementById('result').innerHTML += '<div><a href="https://mnluan.github.io/gameshop/game/' + url + '" target="_blank">'+ data[i].name +'</a></div>';
+            document.getElementById("search_result").style.display = "block"; 
+          }
+        }
+      });
     }
-    console.log(x);
   });
