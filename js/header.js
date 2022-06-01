e2d4615e514d956d1352926ae65c67e8b67d4ebd
Luan Nascimento <luann@id.uff.br>
2022-05-31 23:34:40 -0300

    search avaliable in all pages

--- js/header.js
+++ js/header.js
@@ -2,11 +2,21 @@ function createHeader(){
     document.getElementById('header').innerHTML = '' +
     '<header>' +
         '<a href="./index.html" class="logo"><img src="./img/logo.png" alt="logo"></a>' +
+        '<div id="homebar">' +
+          '<form>' +
+            '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>' +
+            '<input type="search" id="searchbar" placeholder="Search..." onkeydown="autoSearch()">' +
+          '</form>' +
+        '</div>' +
         '<div id="menu-bar"></div>' +
         '<nav class="navbar">' +
           '<a class="cart-menu" href="#"><img src="./img/cart.png" alt="cart"> <div class="count" id="count"></div></a>' +
         '</nav>' +
-    '</header>'
+    '</header>' + 
+    '<section class="search_result" id="search_result">' +
+      '<button onclick="closebtn()"><b>X</b></button>' +
+      '<div id="result"></div>' +
+    '</section>'
 };
   
 createHeader();
--- js/search.js
+++ js/search.js
@@ -9,16 +9,20 @@ function autoSearch(){
   if((x == "") || (x == " ")){
     document.getElementById("search_result").style.display = "none";
     document.getElementById('result').innerHTML = "";
+    list = "";
   }else{
+    var list = "";
     fetch(`https://mnluan.github.io/gameshop/db/games.json`)
       .then((res) => res.json())
       .then((data) => {
         for (i = 0; i < data.length; i++){
           document.getElementById("search_result").style.display = "block"; 
           var string = data[i].name.toUpperCase();
-          if((string.includes(x)) && ((x != "") && (x != " "))){
-            var url = data[i]._id.$oid
-            document.getElementById('result').innerHTML = '<div><a href="https://mnluan.github.io/gameshop/game/' + url + '" target="_blank">'+ data[i].name +'</a></div>';
+          if(string.includes(x)){
+            document.getElementById('result').innerHTML = "";
+            var url = data[i]._id.$oid;
+            list += data[i].name;
+            document.getElementById('result').innerHTML += '<div><a href="https://mnluan.github.io/gameshop/game/' + url + '" target="_blank">'+ data[i].name +'</a></div>';
           }
         }
       });
