bf28ef00528372b6a498639fc1c9aa902760fa2c
Luan Nascimento <luann@id.uff.br>
2022-06-02 22:22:26 -0300

    Update URLs

--- js/list.js
+++ js/list.js
@@ -1,10 +1,10 @@
 var url = window.location.href;
 
-function createHTML(data, i){
-    document.getElementById('list-all').innerHTML += '<div class="game-item">' + 
-    '<a href="./game/' + data[i]._id.$oid +'.html"><div class="game-item">' + 
-    '<img class="logo" src=".'+ data[i].Image + '">' +
-    '<div class="title">' + data[i].name +'</div></a>'+
+function createHTML(i, d){
+    document.getElementById('list-all').innerHTML += '' + 
+    '<a href="./game/' + d[i]._id.$oid +'.html"><div class="game-item">' + 
+    '<img class="logo" src="'+ d[i].Image + '">' +
+    '<div class="title">' + d[i].name +'</div></a>'+
     '</div><br>';
 }
 
@@ -12,55 +12,61 @@ function listgames(){
 fetch(`https://mnluan.github.io/gameshop/db/games.json`)
 .then((res) => res.json())
 .then((data) => {
-    if(url == "https://mnluan.github.io/gameshop/all.html" || url == "https://mnluan.github.io/gameshop/all.html?"){
+    if(url == "https://mnluan.github.io/gameshop/fight.html" || url == "https://mnluan.github.io/gameshop/fight.html?"){
         for (i = 0; i < data.length; i++){
-            createHTML(data, i);
+            createHTML(i, data);
         }
     }
     
     for (i = 0; i < data.length; i++){
         if((url == "https://mnluan.github.io/gameshop/rpg.html") && (data[i].genre == "rpg")){
-            createHTML(data, i);
+            for (i = 0; i < data.length; i++){
+                createHTML(i, data);
+            }
         }
     }
 
     for (i = 0; i < data.length; i++){
         if((url == "https://mnluan.github.io/gameshop/fps.html") && (data[i].genre == "FPS")){
-            document.getElementById('list-all').innerHTML += '<div class="game-item">' + 
-            createHTML(data, i);
+            for (i = 0; i < data.length; i++){
+                createHTML(i, data);
+            }
         }
     }
 
     for (i = 0; i < data.length; i++){
         if((url == "https://mnluan.github.io/gameshop/racing.html") && (data[i].genre == "racing")){
-            document.getElementById('list-all').innerHTML += '<div class="game-item">' + 
-            createHTML(data, i);
+            for (i = 0; i < data.length; i++){
+                createHTML(i, data);
+            }
         }
     }
 
     for (i = 0; i < data.length; i++){
         if((url == "https://mnluan.github.io/gameshop/indie.html") && (data[i].genre == "indie")){
-            document.getElementById('list-all').innerHTML += '<div class="game-item">' + 
-            createHTML(data, i);
+            for (i = 0; i < data.length; i++){
+                createHTML(i, data);
+            }
         }
     }
 
     for (i = 0; i < data.length; i++){
         if((url == "https://mnluan.github.io/gameshop/fight.html") && (data[i].genre == "fight")){
-            document.getElementById('list-all').innerHTML += '<div class="game-item">' + 
-            createHTML(data, i);;
+            for (i = 0; i < data.length; i++){
+                createHTML(i, data);
+            }
         }
     }
 
     for (i = 0; i < data.length; i++){
         if((url == "https://mnluan.github.io/gameshop/strategy.html") && (data[i].genre == "strategy")){
-            document.getElementById('list-all').innerHTML += '<div class="game-item">' + 
-            createHTML(data, i);
+            for (i = 0; i < data.length; i++){
+                createHTML(i, data);
+            }
         }
     }
 
-});
-
+    });
 };
 
 listgames();
